c76d4afad99673ca5fe0e96c95c0b10eb9069795
ArjhanToteck <38510221+ArjhanToteck@users.noreply.github.com>
2024-09-29 00:20:27 -0600

    work on implementing binpacking

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 "use client";
 
+import LoadingBar from "@/src/components/LoadingBar";
 import { BP3D } from "binpackingjs";
 
 const { Item, Bin, Packer } = BP3D;
@@ -10,7 +11,6 @@ export default function Page() {
 	let bricks;
 	let bricksBin = new Bin("bricks", 220, 250, 220);
 	let binPacker = new Packer();
-	binPacker.addBin(bricksBin);
 
 	return (
 		<main>
@@ -22,6 +22,9 @@ export default function Page() {
 				<input id="setId" type="text" autoComplete="off" placeholder="Lego Set ID"></input>
 				<br></br>
 				<button onClick={loadSet}>Load Set</button>
+				<br></br>
+				{/* TODO: figure out how to update loading bar*/}
+				<LoadingBar percentage={50} />
 
 			</section>
 		</main>
@@ -39,42 +42,56 @@ export default function Page() {
 	}
 
 	async function loadBrickStls() {
-
-		// pack items into bin1
-		packer.pack();
-
-		// item1, item2, item3
-		console.log(bin1.items);
-
 		for (let i = 0; i < bricks.length; i++) {
 			const brick = bricks[i];
 
 			// load stl
 			brick.stl = await loadStlFromName(brick.stlName);
 
-			// create brick bin packing item
-			// TODO: why tf is everything multiplied by 100,000 by the library
-			const brickBinItem = new Item(brick.stlName, 5, 5, 5, 5);
-			binPacker.addItem(brickBinItem);
+			// load bounding box for stl
+			brick.dimensions = await loadDimensionsFromName(brick.stlName);
+
+			// loop once for each brick of this type in the set
+			for (let j = 0; j < brick.quantity; j++) {
+				// create brick bin packing item
+				// TODO: why tf is everything multiplied by 100,000 by the library
+				const brickBinItem = new Item("Item " + i, ...brick.dimensions, 0);
+				binPacker.addItem(brickBinItem);
+			}
 		}
 
 		packBricks();
 	}
 
 	async function loadStlFromName(name) {
-		const stlRequest = await fetch(`https://raw.githubusercontent.com/ArjhanToteck/LDraw-Library/main/stl/${name}.stl`, { cache: "no-store" });
+		const response = await fetch(`https://raw.githubusercontent.com/ArjhanToteck/LDraw-Library/main/stl/${name}.stl`, { cache: "no-store" });
 
 		// check if stl exists
-		if (!stlRequest.ok) {
+		if (!response.ok) {
 			return null;
 		} else {
 			// return stl
-			return await stlRequest.text();
+			return await response.text();
+		}
+	}
+
+	async function loadDimensionsFromName(name) {
+		const response = await fetch(`https://raw.githubusercontent.com/ArjhanToteck/LDraw-Library/refs/heads/main/dimensions/${name}.json`, { cache: "no-store" });
+
+		// check if dimensions exist
+		if (!response.ok) {
+			return null;
+		} else {
+			// return dimensions
+			return await response.json();
 		}
 	}
 
 	function packBricks() {
+		binPacker.addBin(bricksBin);
 		binPacker.pack();
 		console.log(bricksBin.items);
+		// TODO: handle overflow, creating multiple bins
+		// TODO: use binpacking info to join everything into a single stl
 	}
 }
